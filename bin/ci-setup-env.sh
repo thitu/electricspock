7f1438c91e25ed71eb7c63e6f1a25f03103c3fd2
Nikolas Thitu <nikolas@hauz.io>
2018-12-14 23:40:38 -0500

    updating env setup script

--- bin/ci-setup-env.sh
+++ bin/ci-setup-env.sh
@@ -4,5 +4,9 @@ set -e
 
 mkdir -p ~/.gradle
 
-echo "BINTRAY_USERNAME=${BINTRAY_USERNAME}" >> ~/.gradle/gradle.properties
-echo "BINTRAY_API_KEY=${BINTRAY_API_KEY}" >> ~/.gradle/gradle.properties
+if [ -n "${BINTRAY_USERNAME}" ] && [ -n "${BINTRAY_API_KEY}" ]; then
+  echo "BINTRAY_USERNAME=${BINTRAY_USERNAME}" >> ~/.gradle/gradle.properties
+  echo "BINTRAY_API_KEY=${BINTRAY_API_KEY}" >> ~/.gradle/gradle.properties
+else
+  echo "Error: missing publishing secrets" && exit -1
+fi
